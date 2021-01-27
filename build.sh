c78dab6b7982991ddac903569b553aedfb37bcfa
Traffic <trafficconegod@gmail.com>
2021-01-26 21:13:16 -0500

    change t_inp to t_vm

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd src
-g++ -o ../out toaster.cpp toast.cpp t_cmp.cpp t_inp.cpp -lstdc++ -Wl,--no-as-needed -ldl
+g++ -o ../out toaster.cpp toast.cpp t_cmp.cpp t_vm.cpp -lstdc++ -Wl,--no-as-needed -ldl
